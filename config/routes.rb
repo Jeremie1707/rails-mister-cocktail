30289e93498230e4ba87b248aa34bfe0ccdc7954
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-09-20 16:44:27 +0200

    adding a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cocktails#index'
   resources :cocktails, except: [:destroy, :edit, :update] do
     resources :doses, except: [:edit, :update, :destroy]
   end

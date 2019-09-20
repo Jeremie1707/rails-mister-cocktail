ef65766cd01a5bb8784c92b2d6c428713e385024
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-09-20 16:06:02 +0200

    app ready before heroku

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,38 +1,35 @@
 class CocktailsController < ApplicationController
-
-  def new
-     @cocktail = Cocktail.new
-  end
-
-  def create
-   @cocktail = Cocktail.new(set_params)
-   if @cocktail.save
-    redirect_to @cocktail, notice: 'The Cocktail was successfully created.'
-    else
-      render :new
-    end
-  end
-
   def index
     @cocktails = Cocktail.all
   end
 
   def show
+    # raise
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
-  # def edit
-  #   @cocktail = Cocktail.find(params[:id])
+  def new
+    @cocktail = Cocktail.new
+  end
 
-  # end
+  def create
+    # raise
+    cocktail = Cocktail.new(strong_params_cocktail)
+    if cocktail.save
+      redirect_to cocktails_path
+      # redirect to show page: redirect_to cocktail_path(cocktail)
+    else
+      render :new
+    end
+  end
 
-  # def update
-  # end
+  private
 
-private
-
-  def set_params
+  def strong_params_cocktail
     params.require(:cocktail).permit(:name)
   end
-
+  # def set_cocktail
+  #   @cocktail = Cocktail.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, except: [:destroy, :edit, :update] do
-    resources :doses, except: [:edit, :update]
-    end
+    resources :doses, except: [:edit, :update, :destroy]
   end
+    resources :doses, only: [:destroy]
+end
+
 
